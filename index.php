10bfe35649617b3e9d625965b6e6488da8537fdc
KumTou <kumtou.lam@gmail.com>
2016-02-18 11:54:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,47 @@
 <?php
  echo "";
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
- <title></title>
- <link href = "css/bootstrap.css" rel = "stylesheet">
+<meta charset="utf-8">
+<title>Golden Peach Forest</title>
+<!-- for FF, Chrome, Opera -->
+<link rel="icon" type="image/png" href="img/icon.png" sizes="16x16">
+<link rel="icon" type="image/png" href="img/icon.png" sizes="32x32">
+<!-- for IE -->
+
+<!--stylesheets -->
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
+<link href = "css/bootstrap.css" rel = "stylesheet">
 </head>
-
 <body>
-The content of the document......
-<a href = "page1.html"> Page1</a>
-
-
+	<div class = 'container'>
+		<div id = "content">
+			<div id = "logo">
+				<h1> Golden Peach Forest </h1>
+			</div>
+			
+			<div class = "topNav">
+				<ul>
+					<li class = "page-collection"> 
+						<a href = "index.html"> Home </a>
+					</li>
+					<li class = "page-collection"> 
+						<a href = "index.html"> Projects </a>
+					</li>
+					<li class = "page-collection"> 
+						<a href = "index.html"> About </a>
+					</li>
+					<li class = "page-collection"> 
+						<a href = "index.html"> Resume </a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src = "js/bootstrap.js"></script>
 </body>
-
 </html>
