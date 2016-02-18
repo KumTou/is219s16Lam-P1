5b37b37916a40df58298c4fcab64495f34e13673
KumTou <kumtou.lam@gmail.com>
2016-02-18 11:04:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-	echo "hello world";
+ echo "";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Title of the document</title>
+ <link href = "css/bootstrap.css" rel = "stylesheet">
+</head>
+
+<body>
+The content of the document......
+<a href = "page1.html"> Page1</a>
+</body>
+
+</html>
