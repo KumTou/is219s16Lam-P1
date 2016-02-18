f2fd64ea69afae4e418645a224288d5eb847f015
KumTou <kumtou.lam@gmail.com>
2016-02-18 11:13:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
 <!DOCTYPE html>
 <html>
 <head>
- <title>Title of the document</title>
+ <title></title>
  <link href = "css/bootstrap.css" rel = "stylesheet">
 </head>
 
 <body>
 The content of the document......
 <a href = "page1.html"> Page1</a>
+
+
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src = "js/bootstrap.js"></script>
 </body>
 
 </html>
